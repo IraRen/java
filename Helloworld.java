3d2f61fb00839272ab7d8d5ff25c3e5db8eedf3a
ChangGeA <changge@changgedeMacBook-Pro.local>
2017-03-17 15:09:47 +0800

    add a java file

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+package qwer;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		
+	}
+
+}
